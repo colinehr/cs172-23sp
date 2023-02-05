006c1b5ec98a9343fe4fa6a1be7df3d00fbc898a
Colin Ehr <colinehr@gmail.com>
2023-02-05 14:13:12 -0800

    add grid and mine drawing

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -16,6 +16,22 @@ public class Minesweeper {
 
     public static void main(String[] args) {
         boolean[][] mines = initMines(10);
+
+        StdDraw.setXscale(0, 8);
+        StdDraw.setYscale(0, 8);
+        // draw grid
+        for (int i = 1; i < 8; i++) {
+            StdDraw.line(i, 0, i, 8);
+            StdDraw.line(0, i, 8, i);
+        }
+        // draw mines
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (mines[row][col]) {
+                    StdDraw.filledCircle(col + 0.5, row + 0.5, 0.25);
+                }
+            }
+        }
     }
 
 }
