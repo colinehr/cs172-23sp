ffe76a922cbdb1699ccf10e78cfbd626013e5b54
Colin Ehr <colinehr@lclark.edu>
2023-02-10 16:44:03 -0800

    in-class work on countNeighboringMines and its test

--- tests/MinesweeperTest.java
+++ tests/MinesweeperTest.java
@@ -1,8 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinesweeperTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void initMines() {
         boolean[][] mines = Minesweeper.initMines(8, 8,10);
         int mineCount = 0;
@@ -14,5 +16,21 @@ class MinesweeperTest {
             }
         }
         assertEquals(10, mineCount);
+    }
+
+    @Test
+    void countNeighboringMines() {
+        // initialize a mines array; notice it will be the same every time
+        boolean[][] mines = new boolean[3][3];
+        mines[0][1] = true;
+        mines[2][2] = true;
+        /* the mines array looks something like:
+            false true  false
+            false false false
+            false false true
+         */
+        assertEquals(2, Minesweeper.countNeighboringMines(mines, 1, 1));
+        assertEquals(1, Minesweeper.countNeighboringMines(mines, 0, 0));
+        // what could one more test look like?
     }
 }
