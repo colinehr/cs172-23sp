11601b464a257fd38a8d53af3a82fa2454a3522a
colinehr <colinehr@lclark.edu>
2023-02-14 11:45:32 -0800

    add call stack example for arrays

--- src/CallStackExample.java
+++ src/CallStackExample.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class CallStackExample {
 
     public static void main(String[] args) {
@@ -5,6 +7,12 @@ public class CallStackExample {
         n = addOne(n);
         int m = addTwo(n);
         int sum = add(n, m);
+
+        int[] arr = onesArray(sum);
+        incrementAll(arr);
+        int[] arr2 = arr;
+        int[] arr3 = Arrays.copyOf(arr, arr.length);
+        arr[0] = -1;
     }
 
     public static int addOne(int n) {
@@ -12,13 +20,27 @@ public class CallStackExample {
     }
 
     public static int addTwo(int n) {
-        addOne(n);
-        addOne(n);
+        n = addOne(n);
+        n = addOne(n);
         return n;
     }
 
     public static int add(int x, int y) {
         return x + y;
+    }
+
+    public static int[] onesArray(int length) {
+        int[] ones = new int[length];
+        for (int i = 0; i < length; i++) {
+            ones[i] = 1;
+        }
+        return ones;
+    }
+
+    public static void incrementAll(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            a[i]++;
+        }
     }
 
 }
