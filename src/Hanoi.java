1b6bfcfeb2b69f490c8efe3cf2cc8bdb80c9c4e9
Colin Ehr <colinehr@lclark.edu>
2023-03-03 15:28:12 -0800

    filled in Hanoi template

--- src/Hanoi.java
+++ src/Hanoi.java
@@ -5,25 +5,32 @@ public class Hanoi {
     }
 
     public static void move2(char source, char destination, char helper) {
-        // move two disks using move1
+        move1(source, helper, destination);
+        move1(source, destination, helper);
+        move1(helper, destination, source);
     }
 
     public static void move3(char source, char destination, char helper) {
-        // move three disks using move2
+        // move three disks using move2 & move1
+        move2(source, helper, destination);
+        move1(source, destination, helper);
+        move2(helper, destination, source);
     }
 
     public static void move(int n, char source, char destination, char helper) {
-        // move n disks using move and move1 (or just printing)
+        // notice this function only depends on itself, not move1, move2, etc.
+        if (n == 1) {
+            StdOut.printf("Move from peg %c to peg %c\n", source, destination);
+            return;
+        }
+        // move n disks using move
+        move(n - 1, source, helper, destination);
+        move(1, source, destination, helper);
+        move(n - 1, helper, destination, source);
     }
 
     public static void main(String[] args) {
-        /*
-        Should output the following:
-            Move from peg A to peg C
-            Move from peg A to peg B
-            Move from peg C to peg B
-         */
-        move2('A', 'B', 'C');
+        move(3,'A', 'B', 'C');
     }
 
 }
