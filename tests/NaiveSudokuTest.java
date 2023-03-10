382efe465228823b91b5833347a89209f12c1a98
colinehr <colinehr@lclark.edu>
2023-03-10 10:28:15 -0800

    fix indentation of test puzzles

--- tests/NaiveSudokuTest.java
+++ tests/NaiveSudokuTest.java
@@ -30,14 +30,14 @@ class NaiveSudokuTest {
     @Test
     void convertsStringToGrid() {
         String correct =    ".1.......\n" +
-                "........8\n" +
-                ".........\n" +
-                ".........\n" +
-                ".........\n" +
-                ".........\n" +
-                ".........\n" +
-                "....5....\n" +
-                ".........\n";
+                            "........8\n" +
+                            ".........\n" +
+                            ".........\n" +
+                            ".........\n" +
+                            ".........\n" +
+                            ".........\n" +
+                            "....5....\n" +
+                            ".........\n";
         int[][] grid = NaiveSudoku.createSquares(correct);
         assertEquals(correct, NaiveSudoku.toString(grid));
     }
@@ -45,14 +45,14 @@ class NaiveSudokuTest {
     @Test
     void rejectsUnsolvablePuzzle() {
         String diagram =    ".12345678\n" +
-                "2........\n" +
-                "3........\n" +
-                "4........\n" +
-                "5........\n" +
-                "6........\n" +
-                "7........\n" +
-                "8........\n" +
-                "9........\n";
+                            "2........\n" +
+                            "3........\n" +
+                            "4........\n" +
+                            "5........\n" +
+                            "6........\n" +
+                            "7........\n" +
+                            "8........\n" +
+                            "9........\n";
         int[][] grid = NaiveSudoku.createSquares(diagram);
         assertFalse(NaiveSudoku.solve(grid));
     }
@@ -61,25 +61,25 @@ class NaiveSudokuTest {
     void findsOneMissingNumber() {
         // This puzzle adapted from www.puzzles.ca/sudoku/
         String diagram =    "897312456\n" +
-                "613594728\n" +
-                "425678913\n" +
-                "941867532\n" +
-                "538921647\n" +
-                "276435.91\n" +
-                "359186274\n" +
-                "764253189\n" +
-                "182749365\n";
+                            "613594728\n" +
+                            "425678913\n" +
+                            "941867532\n" +
+                            "538921647\n" +
+                            "276435.91\n" +
+                            "359186274\n" +
+                            "764253189\n" +
+                            "182749365\n";
         int[][] grid = NaiveSudoku.createSquares(diagram);
         assertTrue(NaiveSudoku.solve(grid));
         String correct =    "897312456\n" +
-                "613594728\n" +
-                "425678913\n" +
-                "941867532\n" +
-                "538921647\n" +
-                "276435891\n" +
-                "359186274\n" +
-                "764253189\n" +
-                "182749365\n";
+                            "613594728\n" +
+                            "425678913\n" +
+                            "941867532\n" +
+                            "538921647\n" +
+                            "276435891\n" +
+                            "359186274\n" +
+                            "764253189\n" +
+                            "182749365\n";
         assertEquals(correct, NaiveSudoku.toString(grid));
     }
 
@@ -87,25 +87,25 @@ class NaiveSudokuTest {
     void findsTwoMissingNumbers() {
         // This puzzle adapted from www.puzzles.ca/sudoku/
         String diagram =    "897312456\n" +
-                "6135..728\n" +
-                "425678913\n" +
-                "941867532\n" +
-                "538921647\n" +
-                "276435891\n" +
-                "359186274\n" +
-                "764253189\n" +
-                "182749365\n";
+                            "6135..728\n" +
+                            "425678913\n" +
+                            "941867532\n" +
+                            "538921647\n" +
+                            "276435891\n" +
+                            "359186274\n" +
+                            "764253189\n" +
+                            "182749365\n";
         int[][] grid = NaiveSudoku.createSquares(diagram);
         assertTrue(NaiveSudoku.solve(grid));
         String correct =    "897312456\n" +
-                "613594728\n" +
-                "425678913\n" +
-                "941867532\n" +
-                "538921647\n" +
-                "276435891\n" +
-                "359186274\n" +
-                "764253189\n" +
-                "182749365\n";
+                            "613594728\n" +
+                            "425678913\n" +
+                            "941867532\n" +
+                            "538921647\n" +
+                            "276435891\n" +
+                            "359186274\n" +
+                            "764253189\n" +
+                            "182749365\n";
         assertEquals(correct, NaiveSudoku.toString(grid));
     }
 
@@ -113,25 +113,25 @@ class NaiveSudokuTest {
     void solvesEasyPuzzle() {
         // This puzzle from www.puzzles.ca/sudoku/
         String diagram =    ".97..24..\n" +
-                "....9..28\n" +
-                "4.567..1.\n" +
-                ".....7...\n" +
-                ".3.......\n" +
-                "..6..5.9.\n" +
-                "...1....4\n" +
-                "764......\n" +
-                "18....36.\n";
+                            "....9..28\n" +
+                            "4.567..1.\n" +
+                            ".....7...\n" +
+                            ".3.......\n" +
+                            "..6..5.9.\n" +
+                            "...1....4\n" +
+                            "764......\n" +
+                            "18....36.\n";
         int[][] grid = NaiveSudoku.createSquares(diagram);
         assertTrue(NaiveSudoku.solve(grid));
         String correct =    "897312456\n" +
-                "613594728\n" +
-                "425678913\n" +
-                "941867532\n" +
-                "538921647\n" +
-                "276435891\n" +
-                "359186274\n" +
-                "764253189\n" +
-                "182749365\n";
+                            "613594728\n" +
+                            "425678913\n" +
+                            "941867532\n" +
+                            "538921647\n" +
+                            "276435891\n" +
+                            "359186274\n" +
+                            "764253189\n" +
+                            "182749365\n";
         assertEquals(correct, NaiveSudoku.toString(grid));
     }
 
@@ -139,25 +139,25 @@ class NaiveSudokuTest {
     void solvesHardPuzzle() {
         // This puzzle from www.puzzles.ca/sudoku/
         String diagram =    "...6.2..3\n" +
-                "..3......\n" +
-                "..8..7.91\n" +
-                "6........\n" +
-                ".12.5.7..\n" +
-                ".....49..\n" +
-                "..5.3....\n" +
-                ".3.5....8\n" +
-                "7....1.6.\n";
+                            "..3......\n" +
+                            "..8..7.91\n" +
+                            "6........\n" +
+                            ".12.5.7..\n" +
+                            ".....49..\n" +
+                            "..5.3....\n" +
+                            ".3.5....8\n" +
+                            "7....1.6.\n";
         int[][] grid = NaiveSudoku.createSquares(diagram);
         assertTrue(NaiveSudoku.solve(grid));
         String correct =    "571692483\n" +
-                "943815627\n" +
-                "268347591\n" +
-                "659723814\n" +
-                "412958736\n" +
-                "387164952\n" +
-                "825436179\n" +
-                "136579248\n" +
-                "794281365\n";
+                            "943815627\n" +
+                            "268347591\n" +
+                            "659723814\n" +
+                            "412958736\n" +
+                            "387164952\n" +
+                            "825436179\n" +
+                            "136579248\n" +
+                            "794281365\n";
         assertEquals(correct, NaiveSudoku.toString(grid));
     }
 
